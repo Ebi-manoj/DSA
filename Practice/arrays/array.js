b6f2ddab283b9f41f328db6d0e184bab4d07846f
Ebi manoj <ebimanoj28@gmail.com>
2025-05-20 20:02:11 +0530

    delete even number

--- Practice/arrays/array.js
+++ Practice/arrays/array.js
@@ -84,3 +84,20 @@ function findMaxSubArray(nums) {
 }
 
 // console.log(findMaxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+///////////////////////////////////////////////////////////////
+////Delete even numbers from an array
+
+function deleteEvenNumber() {
+  const arr = [1, 2, 4, 5, 6, 7, 8, 9, 4, 6];
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      arr[j] = arr[i];
+      j++;
+    }
+  }
+  console.log(arr);
+}
+
+deleteEvenNumber();
