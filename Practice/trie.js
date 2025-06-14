c97239d15a12ea7f06e17278ad6d5dee59978428
Ebi manoj <ebimanoj28@gmail.com>
2025-06-14 20:16:01 +0530

    trie

--- Practice/trie.js
+++ Practice/trie.js
@@ -32,8 +32,59 @@ class Trie {
     }
     return current.endofWord;
   }
+
+  remove(word) {
+    _remove(this.root, word, 0);
+  }
+
+  _remove(current, word, index) {
+    if (index === word.length) {
+      if (!current.endofWord) return false;
+      current.endofWord = false;
+      return Object.keys(current).length == 0;
+    }
+
+    const char = word[index];
+    const node = current.children[char];
+    if (!node) return false;
+
+    const shouldDelete = _remove(node, word, index++);
+
+    if (shouldDelete) {
+      delete current.children[char];
+      return Object.keys(current).length == 0 && !current.endofWord;
+    }
+    return false;
+  }
+
+  autoComplete(prefix) {
+    let current = this.root;
+
+    for (const char of prefix) {
+      if (!current.children[char]) return [];
+      current = current.children[char];
+    }
+    const result = [];
+    this.dfs(current, prefix, result);
+    return result;
+  }
+
+  dfs(node, path, result) {
+    if (node.endofWord) {
+      result.push(path);
+    }
+    for (const char in node.children) {
+      this.dfs(node.children[char], path + char, result);
+    }
+  }
 }
 
 const trie = new Trie();
-trie.insert('Ebi');
+trie.insert('pranav');
+trie.insert('pro');
+trie.insert('promise');
+trie.insert('prithviraj');
+trie.insert('prithi');
 trie.insert('Yohanan');
+
+console.log(trie.autoComplete('pr'));
