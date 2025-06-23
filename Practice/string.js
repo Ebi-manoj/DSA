9171338d77ef0552a1e8d3eb0a9c6c1f1c5e2ad6
Ebi manoj <ebimanoj28@gmail.com>
2025-06-23 21:02:14 +0530

    roman to integer

--- Practice/string.js
+++ Practice/string.js
@@ -149,3 +149,27 @@ const mergeAlternately = function (word1, word2) {
   }
   return output;
 };
+
+/////////////////////////////////////////////////////
+/////ROMAN TO INTEGERS
+const romanToInt = function (s) {
+  const map = new Map();
+  map.set('I', 1);
+  map.set('V', 5);
+  map.set('X', 10);
+  map.set('L', 50);
+  map.set('C', 100);
+  map.set('D', 500);
+  map.set('M', 1000);
+  let output = 0;
+  let lastPosition = 'I';
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (map.get(s[i]) < map.get(lastPosition)) {
+      output -= map.get(s[i]);
+    } else {
+      output += map.get(s[i]);
+      lastPosition = s[i];
+    }
+  }
+  return output;
+};
